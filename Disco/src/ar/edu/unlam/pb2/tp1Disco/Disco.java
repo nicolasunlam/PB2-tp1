5f68b84ec3646b2859a1470e13aa2f0bbed90ae1
FaakaFernandez <facufern@hotmail.com>
2017-04-21 17:40:39 -0300

    Update Disco.java

--- Disco/src/ar/edu/unlam/pb2/tp1Disco/Disco.java
+++ Disco/src/ar/edu/unlam/pb2/tp1Disco/Disco.java
@@ -1,73 +1,60 @@
-package ar.edu.unlam.pb2.disco;
+package ar.edu.unlam.pb2.tp1Disco;
+
+
 
 public class Disco {
-	private Double diametroMayor;
-	private Double diametroMenor;
-	private Double radioExterno;
-	private Double radioInterno;
-	private Double superficie;
-	private Double perimetroExterno;
-	private Double perimetroInterno;
+private Double diametroMayor;
+private Double diametroMenor;
+private Double radioExterno;
+private Double radioInterno;
+private Double superficie;
+private Double perimetroExterno;
+private Double perimetroInterno;
 
-	public Disco(Double diametroMayor, Double diametroMenor) {
-		this.diametroMayor = diametroMayor;
-		this.diametroMenor = diametroMenor;
-	}
-
-	public Double getPerimetroExterno() {
-		return perimetroExterno;
-	}
-
-	public void setPerimetroExterno(Double perimetroExterno) {
-		this.perimetroExterno = 2 * 3.14 * radioExterno;
-	}
-
-	public Double getPerimetroInterno() {
-		return perimetroInterno;
-	}
-
-	public void setPerimetroInterno(Double perimetroInterno) {
-		this.perimetroInterno = 2 * 3.14 * radioInterno;
-	}
-
-	public Double getDiametroMayor() {
-		return diametroMayor;
-	}
-
-	public void setDiametroMayor(Double diametroMayor) {
-		this.diametroMayor = diametroMayor;
-	}
-
-	public Double getDiametroMenor() {
-		return diametroMenor;
-	}
-
-	public void setDiametroMenor(Double diametroMenor) {
-		this.diametroMenor = diametroMenor;
-	}
-
-	public Double getRadioExterno() {
-		return radioExterno;
-	}
-
-	public void setRadioExterno(Double diametroMayor, Double diametroMenor) {
-		this.radioExterno = (diametroMayor + diametroMenor) / 2;
-	}
-
-	public Double getRadioInterno() {
-		return radioInterno;
-	}
-
-	public void setRadioInterno(Double diametroMayor, Double diametroMenor) {
-		this.radioInterno = (diametroMayor - diametroMenor) / 2;
-	}
-
-	public Double getSuperficie() {
-		return superficie;
-	}
-
-	public void setSuperficie(Double diametroMayor, Double diametroMenor) {
-		this.superficie = 3.14 * ((diametroMayor + diametroMenor) * (diametroMayor + diametroMenor));
-	}
-
+public Disco(Double diametroMayor, Double diametroMenor) {
+this.diametroMayor = diametroMayor;
+this.diametroMenor = diametroMenor;
 }
+
+public Double getPerimetroExterno() {
+return perimetroExterno;
+}
+
+public void setPerimetroExterno(Double radioExterno) {
+this.perimetroExterno = 2 * 3.14 * radioExterno;
+}
+
+public Double getPerimetroInterno() {
+return perimetroInterno;
+}
+
+public void setPerimetroInterno(Double radioInterno) {
+this.perimetroInterno = 2 * 3.14 * radioInterno;
+}
+
+
+
+public Double getRadioExterno() {
+return radioExterno;
+}
+
+public void setRadioExterno(Double diametroMayor, Double diametroMenor) {
+this.radioExterno = ((diametroMayor - diametroMenor)+(diametroMayor+diametroMenor))/4;
+}
+
+public Double getRadioInterno() {
+return radioInterno;
+}
+
+public void setRadioInterno(Double diametroMayor, Double diametroMenor) {
+this.radioInterno = ((diametroMayor+diametroMenor)-(diametroMayor-diametroMenor))/4;
+}
+
+public Double getSuperficie() {
+return superficie;
+}
+
+public void setSuperficie(Double diametroMayor, Double diametroMenor) {
+this.superficie = (3.14 *diametroMayor)-(3.14*diametroMenor);
+}
+}//
